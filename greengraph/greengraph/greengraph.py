918cd29c2413e6834051957baf2095dd4fc773d1
Conor Mc Keever <conormckeever92@gmail.com>
2017-01-05 13:54:59 +0000

    broken script into classes and added executable and entry point script stub

--- greengraph/greengraph/greengraph.py
+++ greengraph/greengraph/greengraph.py
@@ -7,10 +7,12 @@ from StringIO import StringIO
 from argparse import ArgumentParser
 
 
-parser = ArgumentParser(description = 'Plot a measure of the green land between two locations')
+parser = ArgumentParser(description = 'Plot proportion of green pixels in a series of sattelite images between two specified points')
 parser.add_argument('start_point',type=str,help = 'Starting location')
 parser.add_argument('end_point',type=str,help = 'ending location')
 parser.add_argument('steps',type=int,help = 'number of steps between locations')
+parser.add_argument('output_name',type=int,help = 'filename of graph output')
+
 
 
 class Greengraph(object):
@@ -87,9 +89,9 @@ arguments = parser.parse_args()
 mygraph=Greengraph(arguments.start_point,arguments.end_point)
 data = mygraph.green_between(arguments.steps)
 
-
 plt.plot(data)
 plt.show()
+plt.savefig("greengraph.png")
 
 
 
